a9c786d9a7c030ffe6ae111bc1c81facecef9acf
espenb08 <espenb08.kodehode@gmail.com>
2024-03-18 11:31:41 +0100

    updated commands

--- index.js
+++ index.js
@@ -21,10 +21,16 @@ client.on("message", (channel, tags, message, self) => {
   // Ignore echoed messages.
   if (self) return;
 
-  if (message.toLowerCase() === "hey") {
-    client.say(channel, `@${tags.username}, Yo what's up`);
+  if (message.toLowerCase() === "hey" || message === "Heyge") {
+    client.say(channel, `Heyge @${tags.username}`);
   }
 if (message.toLowerCase() === "give up") {
   client.say(channel, `yeah @${tags.username} you should give up`);
+}
+if (message.includes("calc") || message.includes("dpscalc")) {
+  client.say(
+    channel,
+    `hey @${tags.username} here is a link for the dps calc https://tools.runescape.wiki/osrs-dps/`
+  );
 }
 });
