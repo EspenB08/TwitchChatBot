ad9caeeb61b7ffd46dde138d0a8bb379388cb411
espenb08 <espenb08.kodehode@gmail.com>
2024-03-20 10:27:31 +0100

    fixed command bugs

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = new tmi.Client({
     username: "NunuLord",
     password: process.env.TOKEN,
   },
-  channels: ["w4rz0r"],
+  channels: ["NunuLord"],
 });
 
 client.connect();
@@ -21,37 +21,37 @@ client.on("message", (channel, tags, message, self) => {
   // Ignore echoed messages.
   if (self) return;
 
-  if (message.toLowerCase() === "hey" || message === "Heyge") {
-    client.say(channel, `Heyge @${tags.username}`);
+  if (message.toLowerCase() === "hey" || message === "Heyge" ||message.toLowerCase() === "gm") {
+    client.say(channel, `Gmge @${tags.username}`);
   }
 if (message.toLowerCase() === "give up") {
   client.say(channel, `yeah @${tags.username} you should give up`);
 }
-if (message.includes("calc") || message.includes("dpscalc")) {
+if (message.includes("!calc") || message.includes("!dpscalc")) {
   client.say(
     channel,
     `hey @${tags.username} here is a link for the dps calc https://tools.runescape.wiki/osrs-dps/`
   );
 }
-if (message.includes(`${channel.slice(1)}`)){
-  client.say(channel, `hvis @${channel.slice(1)} reads this he lost`)
-}
+// if (message.includes(`${channel.slice(1)}`)){
+//   client.say(channel, `hvis @${channel.slice(1)} reads this he lost`)
+// }
 const messageToArray = message.split(" ");
 const messageResponse = messageToArray[1];
 function rng(){
 rngRoll = Math.ceil(Math.random() * 100)+"%";
 
 }
-if (message.includes("!hate")){
+if (message.substring(0, 5) === "!hate"){
   rng()
 client.say(channel, `there is a ${rngRoll} chance of hate between ${tags.username} and ${messageResponse}`)
 }
-if (message.includes(`!love`)){
+if (message.substring(0, 5) === "!love"){
   rng()
   client.say(channel, `there is a ${rngRoll} chance of love between ${tags.username} and ${messageResponse}`)
   
 }
-if (message.includes(`!opgg`)){
+if (message.substring(0, 5) ==="!opgg"){
   client.say(
     channel,
     `here is a link to their opgg https://www.op.gg/summoners/euw/${messageResponse.replace("#", "-")}`
